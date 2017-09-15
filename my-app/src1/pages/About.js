8d79277a0c9dd06ee313f17cb7d371818332e71f
Administrator <1187364874@qq.com>
2017-09-15 16:39:35 +0800

    react-router4.0.2体验   需安装react-router-dom

---
+++ my-app/src1/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const About =()=>{
+    return(
+        <div>我是about页面</div>
+    )
+}
+export default About;
---
+++ my-app/src1/pages/Error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Error=()=>{
+    return (
+        <div>我是错误页面</div>
+    )
+}
+export default Error;
